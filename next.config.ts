516921f8729b2a0e264036efa3f23f33963ad385
Yasin <skyasin4221@gmail.com>
2026-06-08 23:45:12 +0100

    Add www domain redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.grillzoneoriginal.co.uk"
+          }
+        ],
+        destination: "https://grillzoneoriginal.co.uk/:path*",
+        permanent: true
+      }
+    ];
+  },
   images: {
     remotePatterns: [
       {
